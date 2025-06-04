72ea70a6d14dd2401e547e2d438256b88e24617e
SeongminKimKR <gvf156@gmail.com>
2025-06-04 23:33:08 +0900

    typo: 에러 코드 메시지 오타 수정

--- src/main/kotlin/com/yapp/demo/common/exception/ErrorCode.kt
+++ src/main/kotlin/com/yapp/demo/common/exception/ErrorCode.kt
@@ -5,6 +5,6 @@ enum class ErrorCode(
     val code: String,
     val message: String,
 ) {
-    BAD_REQUEST(400, "C-400", "잘못된 요청입니다.."),
+    BAD_REQUEST(400, "C-400", "잘못된 요청입니다."),
     INTERNAL_SERVER_ERROR(500, "C-500", "예기치 못한 서버 오류가 발생했습니다."),
 }
