7156aee0487350a9004b39a4a95909b8fc1667f4
ay-eonii <naayen@naver.com>
2025-06-14 16:43:59 +0900

    fix: UserInfo data class로 변경

--- src/main/kotlin/com/yapp/demo/common/security/oauth2/userinfo/AppleUserInfo.kt
+++ src/main/kotlin/com/yapp/demo/common/security/oauth2/userinfo/AppleUserInfo.kt
@@ -2,14 +2,14 @@ package com.yapp.demo.common.security.oauth2.userinfo
 
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 
-class AppleUserInfo(
+data class AppleUserInfo(
     private val attributes: Map<String, Any>,
 ) : OAuth2UserInfo {
     private val attrs = attributes
     override val id: String = attrs["response"] as String
     override val email: String = attrs["email"] as String
 
-    override fun getName(): String = attrs["name"] as String
+    override fun getName(): String = (attrs["name"] as? String) ?: ""
 
     override fun getAttributes(): Map<String, Any> = attributes
 
