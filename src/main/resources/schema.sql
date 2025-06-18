f3baa44cb62d95d8d400778083046ea05c3d4795
SeongminKimKR <gvf156@gmail.com>
2025-06-18 21:10:14 +0900

    docs: 유저 테이블 DDL 작성

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE user (
+    user_id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    auth_email VARCHAR(255) NOT NULL,
+    social_provider VARCHAR(50) NOT NULL,
+    role VARCHAR(50) NOT NULL,
+    deleted_at DATETIME NULL,
+    status VARCHAR(50) NOT NULL,
+    nickname VARCHAR(50) NULL,
+    created_at DATETIME NULL,
+    updated_at DATETIME NULL
+);
