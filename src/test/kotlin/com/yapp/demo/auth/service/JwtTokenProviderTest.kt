4913ae8e6c8df6322d277a1961044d7915bb1295
SeongminKimKR <gvf156@gmail.com>
2025-06-20 23:17:56 +0900

    test: 테스트 누락 항목 추가

--- src/test/kotlin/com/yapp/demo/auth/service/JwtTokenProviderTest.kt
+++ src/test/kotlin/com/yapp/demo/auth/service/JwtTokenProviderTest.kt
@@ -3,16 +3,23 @@ package com.yapp.demo.auth.service
 import com.yapp.demo.common.config.properties.JwtProperties
 import com.yapp.demo.common.constants.TOKEN_TYPE_ACCESS
 import com.yapp.demo.common.constants.TOKEN_TYPE_REFRESH
+import com.yapp.demo.common.enums.Role
+import com.yapp.demo.common.enums.SocialProvider
 import com.yapp.demo.common.exception.CustomException
 import com.yapp.demo.common.exception.ErrorCode
 import com.yapp.demo.user.infrastructure.UserReader
+import com.yapp.demo.user.model.User
+import com.yapp.demo.user.model.UserStatus
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.Mockito.`when`
 import org.mockito.junit.jupiter.MockitoExtension
 import org.mockito.kotlin.mock
+import org.springframework.security.core.authority.SimpleGrantedAuthority
 import kotlin.test.Test
+import kotlin.test.assertTrue
 
 @ExtendWith(MockitoExtension::class)
 class JwtTokenProviderTest {
@@ -77,5 +84,42 @@ class JwtTokenProviderTest {
             }
 
         assertEquals(ErrorCode.TOKEN_EXPIRED, exception.errorCode)
+    }
+
+    @Test
+    fun `getAuthentication은 유효한 userId에 대해 Authentication 객체를 반환해야 한다`() {
+        // given
+        val userId = 123L
+        val user =
+            User(
+                id = userId,
+                authEmail = "email@email.com",
+                socialProvider = SocialProvider.KAKAO,
+                role = Role.USER,
+                status = UserStatus.ACTIVE,
+            )
+
+        `when`(userReader.findById(userId)).thenReturn(user)
+
+        // when
+        val authentication = jwtTokenProvider.getAuthentication(userId)
+
+        // then
+        assertEquals(userId, authentication.principal)
+        assertTrue(authentication.authorities.contains(SimpleGrantedAuthority(Role.USER.name)))
+        assertEquals(null, authentication.credentials)
+    }
+
+    @Test
+    fun `JwtTokenProvider는 토큰으로 부터 만료시간을 추출할 수 있다`() {
+        val userId = 12345L
+
+        val accessToken = jwtTokenProvider.generateAccessToken(userId)
+        val expiration = jwtTokenProvider.extractExpiration(accessToken)
+
+        val now = System.currentTimeMillis()
+        val expiryWithinRange = expiration in (now + 1000)..(now + jwtProperties.accessTokenExpiryTime * 1000)
+
+        assertEquals(true, expiryWithinRange)
     }
 }
