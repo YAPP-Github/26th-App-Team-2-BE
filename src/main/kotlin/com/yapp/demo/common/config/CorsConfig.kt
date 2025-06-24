a4d0f7d9bfdf76bf3de0f65953d40291d5443269
ay-eonii <naayen@naver.com>
2025-06-24 16:42:50 +0900

    refactor: cors 허용 주소 환경변수로 관리

--- src/main/kotlin/com/yapp/demo/common/config/CorsConfig.kt
+++ src/main/kotlin/com/yapp/demo/common/config/CorsConfig.kt
@@ -1,5 +1,6 @@
 package com.yapp.demo.common.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.cors.CorsConfiguration
@@ -8,10 +9,13 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 class CorsConfig {
+    @Value("\${cors.allowed-origins}")
+    private lateinit var allowedOrigins: String
+
     @Bean
     fun corsConfigurationSource(): CorsConfigurationSource {
         val configuration = CorsConfiguration()
-        configuration.allowedOriginPatterns = ALLOWED_ORIGINS.toList()
+        configuration.allowedOriginPatterns = allowedOrigins.split(",").map { it.trim() }
         configuration.allowedMethods = ALLOWED_METHODS.toList()
         configuration.allowedHeaders = listOf("*")
         configuration.addExposedHeader(AUTHORIZATION_HEADER)
@@ -23,7 +27,6 @@ class CorsConfig {
     }
 
     companion object {
-        private val ALLOWED_ORIGINS = arrayOf("http://35.212.131.175/", "http://localhost:*")
         private val ALLOWED_METHODS = arrayOf("OPTIONS", "GET", "POST", "PUT", "DELETE", "PATCH")
         private const val AUTHORIZATION_HEADER = "Authorization"
         private const val MAX_AGE = 3600L
