6220c02f099176f5fbd1068629900687e6eccfe0
SeongminKimKR <gvf156@gmail.com>
2025-06-26 00:05:34 +0900

    test: 애플키 모킹 처리

--- src/test/kotlin/com/yapp/demo/DemoApplicationTests.kt
+++ src/test/kotlin/com/yapp/demo/DemoApplicationTests.kt
@@ -2,9 +2,14 @@ package com.yapp.demo
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.bean.override.mockito.MockitoBean
+import java.security.PrivateKey
 
 @SpringBootTest
 class DemoApplicationTests {
+    @MockitoBean
+    private lateinit var applePrivateKey: PrivateKey
+
     @Test
     fun contextLoads() {
     }
