e01065ef0e31f1561cd54a08a3ef93d6f66476a9
ay-eonii <naayen@naver.com>
2025-06-25 22:12:19 +0900

    feat: 애플로그인 설정값

---
+++ src/main/kotlin/com/yapp/demo/auth/external/apple/AppleProperties.kt
@@ -0,0 +1,14 @@
+package com.yapp.demo.auth.external.apple
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.stereotype.Component
+
+@Component
+@ConfigurationProperties(prefix = "oauth.apple")
+data class AppleProperties(
+    var clientId: String = "",
+    var teamId: String = "",
+    var keyId: String = "",
+    var audience: String = "",
+    var authKeyPath: String = "",
+)
