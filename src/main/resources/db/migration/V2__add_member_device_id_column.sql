26561e4ba290e59d6325cacfe1e9b9d6690d82f3
SeongminKimKR <gvf156@gmail.com>
2025-07-02 21:54:08 +0900

    chore: 디바이스 식별자 유니크 제약 조건 추가#21

--- src/main/resources/db/migration/V2__add_member_device_id_column.sql
+++ src/main/resources/db/migration/V2__add_member_device_id_column.sql
@@ -2,4 +2,7 @@ ALTER TABLE `member`
 ADD COLUMN device_id VARCHAR(255) NOT NULL AFTER member_id;
 
 ALTER TABLE `member`
+ADD CONSTRAINT uq_member_device_id UNIQUE (device_id);
+
+ALTER TABLE `member`
 DROP COLUMN deleted_at;
