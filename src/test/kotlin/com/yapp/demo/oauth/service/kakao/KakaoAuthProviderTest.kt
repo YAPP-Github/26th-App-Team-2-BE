5f2ece677e4cb380b9ed16f21a53034b4a167332
SeongminKimKR <gvf156@gmail.com>
2025-07-03 01:57:59 +0900

    test: KakaoAuthProviderTest 작성

---
+++ src/test/kotlin/com/yapp/demo/oauth/service/kakao/KakaoAuthProviderTest.kt
@@ -0,0 +1,88 @@
+package com.yapp.demo.oauth.service.kakao
+
+import com.yapp.demo.oauth.infrastructure.feign.kakao.KakaoApiFeignClient
+import com.yapp.demo.oauth.infrastructure.feign.kakao.KakaoAuthFeignClient
+import com.yapp.demo.oauth.infrastructure.feign.kakao.response.KakaoTokenResponse
+import com.yapp.demo.oauth.infrastructure.feign.kakao.response.KakaoUnlinkResponse
+import com.yapp.demo.oauth.infrastructure.feign.kakao.response.KakaoUserInfoResponse
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.mockito.kotlin.any
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+
+class KakaoAuthProviderTest {
+    private val kakaoProperties = mock<KakaoProperties>()
+    private val kakaoAuthFeignClient = mock<KakaoAuthFeignClient>()
+    private val kakaoApiFeignClient = mock<KakaoApiFeignClient>()
+    private val kakaoAuthProvider =
+        KakaoAuthProvider(
+            kakaoProperties,
+            kakaoAuthFeignClient,
+            kakaoApiFeignClient,
+        )
+
+    @Test
+    fun `액세스 토큰을 조회한다`() {
+        val code = "authorizationCode"
+        val response =
+            KakaoTokenResponse(
+                accessToken = "accessToken",
+                tokenType = "access",
+                refreshToken = "refreshToken",
+                expiresIn = 1L,
+                scope = null,
+                refreshTokenExpiresIn = 2L,
+            )
+
+        whenever(
+            kakaoAuthFeignClient.getToken(
+                "authorization_code",
+                kakaoProperties.clientId,
+                kakaoProperties.redirectUri,
+                code,
+            ),
+        ).thenReturn(response)
+
+        val result = kakaoAuthProvider.getAccessToken(code)
+
+        assertThat(result).isEqualTo(response.accessToken)
+    }
+
+    @Test
+    fun `유저 정보를 조회한다`() {
+        val accessToken = "access-token"
+        val expected =
+            KakaoUserInfoResponse(
+                id = "kakao-id",
+                kakaoAccount = KakaoUserInfoResponse.KakaoAccount("kakao@email.com"),
+            )
+
+        whenever(kakaoApiFeignClient.getUserInfo(any())).thenReturn(expected)
+
+        val result = kakaoAuthProvider.getUserInfo(accessToken)
+
+        assertThat(result.id).isEqualTo(expected.id)
+        assertThat(result.email).isEqualTo(expected.kakaoAccount.email)
+    }
+
+    @Test
+    fun `탈퇴를 위해 연결을 끊는다`() {
+        val code = "authorizationCode"
+        val response =
+            KakaoTokenResponse(
+                accessToken = "accessToken",
+                tokenType = "access",
+                refreshToken = "refreshToken",
+                expiresIn = 1L,
+                scope = null,
+                refreshTokenExpiresIn = 2L,
+            )
+
+        whenever(kakaoAuthFeignClient.getToken(any(), any(), any(), any())).thenReturn(response)
+        whenever(kakaoApiFeignClient.unlink(response.accessToken)).thenReturn(KakaoUnlinkResponse(123L))
+
+        assertDoesNotThrow { kakaoAuthProvider.withdraw(code) }
+    }
+}
