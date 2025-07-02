1e0676eb31f3d393a2d0a6830e71b96b589d0661
SeongminKimKR <gvf156@gmail.com>
2025-07-02 22:22:11 +0900

    fix: unresolved 에러 수정

--- src/main/kotlin/com/yapp/demo/auth/service/AuthUseCase.kt
+++ src/main/kotlin/com/yapp/demo/auth/service/AuthUseCase.kt
@@ -3,6 +3,7 @@ package com.yapp.demo.auth.service
 import com.yapp.demo.auth.dto.request.OAuthLoginRequest
 import com.yapp.demo.auth.dto.response.OAuthLoginResponse
 import com.yapp.demo.auth.dto.response.RefreshTokenResponse
+import com.yapp.demo.common.enums.SocialProvider
 
 interface AuthUseCase {
     fun login(request: OAuthLoginRequest): OAuthLoginResponse
