553257ef0ebdcea9f4d182165fe16c8775c59631
SeongminKimKR <gvf156@gmail.com>
2025-07-04 00:36:29 +0900

    refactor: 로그인시 db 트랜잭션 분리

--- src/main/kotlin/com/yapp/demo/auth/service/AuthService.kt
+++ src/main/kotlin/com/yapp/demo/auth/service/AuthService.kt
@@ -14,6 +14,7 @@ import com.yapp.demo.common.security.getMemberId
 import com.yapp.demo.member.infrastructure.MemberReader
 import com.yapp.demo.member.infrastructure.MemberWriter
 import com.yapp.demo.member.model.Member
+import com.yapp.demo.oauth.model.OAuthUserInfo
 import com.yapp.demo.oauth.service.OAuthProvider
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -28,7 +29,6 @@ class AuthService(
     private val refreshTokenRepository: RefreshTokenRepository,
     private val blackListRepository: BlackListRepository,
 ) : AuthUseCase {
-    @Transactional
     override fun login(request: OAuthLoginRequest): OAuthLoginResponse {
         val authProvider =
             findProvider(request.provider)
@@ -37,19 +37,7 @@ class AuthService(
         val authToken = authProvider.getAccessToken(request.authorizationCode)
         val userInfo = authProvider.getUserInfo(authToken)
 
-        var member = memberReader.findByDeviceId(request.deviceId)
-
-        if (member == null) {
-            member =
-                memberWriter.save(
-                    Member.create(
-                        deviceId = request.deviceId,
-                        oAuthUserInfo = userInfo,
-                        socialProvider = request.provider,
-                        role = Role.USER,
-                    ),
-                )
-        }
+        val member = findOrCreateMember(request.deviceId, request.provider, userInfo)
 
         val accessToken = jwtTokenProvider.generateAccessToken(member.id)
         val refreshToken = jwtTokenProvider.generateRefreshToken(member.id)
@@ -95,6 +83,23 @@ class AuthService(
 
         authProvider.withdraw(credential)
         memberWriter.delete(getMemberId())
+    }
+
+    @Transactional
+    fun findOrCreateMember(
+        deviceId: String,
+        provider: SocialProvider,
+        userInfo: OAuthUserInfo,
+    ): Member {
+        return memberReader.findByDeviceId(deviceId)
+            ?: memberWriter.save(
+                Member.create(
+                    deviceId = deviceId,
+                    oAuthUserInfo = userInfo,
+                    socialProvider = provider,
+                    role = Role.USER,
+                ),
+            )
     }
 
     private fun findProvider(socialType: SocialProvider): OAuthProvider? =
