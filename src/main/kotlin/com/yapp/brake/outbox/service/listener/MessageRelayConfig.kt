772ddaedc50ed88958d1516aa4ea6b454c54e12d
SeongminKimKR <gvf156@gmail.com>
2025-07-08 21:12:00 +0900

    feat: 메시지 릴레이 스레드풀 설정 추가(프로세스 종료 후, 스레드 꽉 찼을 때) #33

--- src/main/kotlin/com/yapp/brake/outbox/service/listener/MessageRelayConfig.kt
+++ src/main/kotlin/com/yapp/brake/outbox/service/listener/MessageRelayConfig.kt
@@ -7,6 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
 import java.util.concurrent.Executor
 import java.util.concurrent.Executors
+import java.util.concurrent.ThreadPoolExecutor.CallerRunsPolicy
 
 @EnableAsync
 @Configuration
@@ -16,12 +17,22 @@ class MessageRelayConfig {
     fun messageRelayPublishEventExecutor(): Executor =
         ThreadPoolTaskExecutor()
             .apply {
-                corePoolSize = 20
-                maxPoolSize = 50
-                queueCapacity = 100
+                corePoolSize = POOL_SIZE
+                maxPoolSize = MAX_POOL_SIZE
+                queueCapacity = QUEUE_SIZE
                 setThreadNamePrefix("pub-event-")
+                setWaitForTasksToCompleteOnShutdown(true)
+                setAwaitTerminationSeconds(AWAIT_TERMINATION_SECONDS)
+                setRejectedExecutionHandler(CallerRunsPolicy())
             }
 
     @Bean
     fun messageRelayPublishPendingEventExecutor(): Executor = Executors.newSingleThreadScheduledExecutor()
+
+    companion object {
+        private const val POOL_SIZE = 20
+        private const val MAX_POOL_SIZE = 50
+        private const val QUEUE_SIZE = 100
+        private const val AWAIT_TERMINATION_SECONDS = 10
+    }
 }
