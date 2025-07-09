e9db2dc046fc32af62e4cd0fd87c930ca98e7124
SeongminKimKR <gvf156@gmail.com>
2025-07-09 23:35:43 +0900

    test: MemberService.delete() 상호 검증 테스트 추가

--- src/test/kotlin/com/yapp/brake/member/service/MemberServiceTest.kt
+++ src/test/kotlin/com/yapp/brake/member/service/MemberServiceTest.kt
@@ -16,6 +16,7 @@ import org.junit.jupiter.api.assertThrows
 import org.mockito.Mockito.mock
 import org.mockito.kotlin.any
 import org.mockito.kotlin.doNothing
+import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
@@ -90,5 +91,9 @@ class MemberServiceTest {
         doNothing().whenever(outboxEventPublisher).publish(EventType.MEMBER_DELETED, payload)
 
         memberService.delete(member.id)
+
+        verify(memberReader).getById(member.id)
+        verify(memberWriter).delete(member.id)
+        verify(outboxEventPublisher).publish(EventType.MEMBER_DELETED, payload)
     }
 }
