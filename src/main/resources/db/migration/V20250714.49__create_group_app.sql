70d1bfadb4e3896c725047c80938c56cb50466c7
SeongminKimKR <gvf156@gmail.com>
2025-07-14 22:47:51 +0900

    chore: group_app 테이블에 유니크 인덱스 추가 #49

--- src/main/resources/db/migration/V20250714.49__create_group_app.sql
+++ src/main/resources/db/migration/V20250714.49__create_group_app.sql
@@ -3,7 +3,8 @@ CREATE TABLE group_app (
     group_id        bigint        NOT NULL,
     app_id          VARCHAR(100)  NOT NULL,
     created_at      DATETIME      NOT NULL,
-    updated_at      DATETIME      NOT NULL
+    updated_at      DATETIME      NOT NULL,
+    UNIQUE KEY uk_group_app_group_id_app_id (group_id, app_id)
 );
 
 CREATE INDEX idx_group_app_group_id ON group_app (group_id);
