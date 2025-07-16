1948e7b63f1dfe6ef5eb60af3953ee5934f61438
SeongminKimKR <gvf156@gmail.com>
2025-07-17 00:14:43 +0900

    test: 서비스 테스트 작성 #50

---
+++ src/test/kotlin/com/yapp/brake/support/fixture/dto/group/UpdateGroupAppRequestFixture.kt
@@ -0,0 +1,11 @@
+package com.yapp.brake.support.fixture.dto.group
+
+import com.yapp.brake.group.dto.request.UpdateGroupAppRequest
+
+fun updateGroupAppRequestFixture(
+    groupAppId: Long = 1L,
+    name: String = "카카오톡",
+) = UpdateGroupAppRequest(
+    groupAppId = groupAppId,
+    name = name,
+)
