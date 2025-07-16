c73f25fd55a5b26c43df317d61c4fc019da9bdaf
SeongminKimKR <gvf156@gmail.com>
2025-07-16 23:59:53 +0900

    fix: 중복 값 제거 #50

--- src/main/kotlin/com/yapp/brake/group/dto/request/UpdateGroupRequest.kt
+++ src/main/kotlin/com/yapp/brake/group/dto/request/UpdateGroupRequest.kt
@@ -1,14 +1,9 @@
 package com.yapp.brake.group.dto.request
 
 import jakarta.validation.constraints.NotBlank
-import jakarta.validation.constraints.NotNull
-import jakarta.validation.constraints.Positive
 import jakarta.validation.constraints.Size
 
 data class UpdateGroupRequest(
-    @field:NotNull
-    @field:Positive
-    val groupId: Long,
     @field:NotBlank
     @field:Size(min = 2, max = 10)
     val name: String,
--- src/test/kotlin/com/yapp/brake/group/dto/request/UpdateGroupRequestTest.kt
+++ src/test/kotlin/com/yapp/brake/group/dto/request/UpdateGroupRequestTest.kt
@@ -12,7 +12,7 @@ class UpdateGroupRequestTest {
     inner class UpdateGroupRequestBoundaryTest {
         @Test
         fun `닉네임의 길이가 2 미만이면 예외가 발생한다`() {
-            val request = UpdateGroupRequest(1L, "A")
+            val request = UpdateGroupRequest("A")
             val violations = validator.validate(request)
 
             assertThat(violations).anyMatch { it.propertyPath.toString() == "name" }
@@ -20,22 +20,10 @@ class UpdateGroupRequestTest {
 
         @Test
         fun `닉네임의 길이가 10 초과면 예외가 발생한다`() {
-            val request = UpdateGroupRequest(1L, "ABCDEFGHIJK")
+            val request = UpdateGroupRequest("ABCDEFGHIJK")
             val violations = validator.validate(request)
 
             assertThat(violations).anyMatch { it.propertyPath.toString() == "name" }
         }
-    }
-
-    @Test
-    fun `괸리 앱 그룹 식별자는 양수가 아니면 예외가 발생 한다`() {
-        val request1 = UpdateGroupRequest(0L, "ABCDE")
-        val request2 = UpdateGroupRequest(-1L, "ABCDE")
-
-        val violations1 = validator.validate(request1)
-        val violations2 = validator.validate(request2)
-
-        assertThat(violations1).anyMatch { it.propertyPath.toString() == "groupId" }
-        assertThat(violations2).anyMatch { it.propertyPath.toString() == "groupId" }
     }
 }
