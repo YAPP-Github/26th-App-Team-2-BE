7cb7bf9741365f7995ca9e62c49b83aad95e0415
SeongminKimKR <gvf156@gmail.com>
2025-07-18 00:33:56 +0900

    fix: 관리 앱 수정 dto validation 변경 #50

--- src/main/kotlin/com/yapp/brake/group/dto/request/UpdateGroupAppRequest.kt
+++ src/main/kotlin/com/yapp/brake/group/dto/request/UpdateGroupAppRequest.kt
@@ -2,12 +2,12 @@ package com.yapp.brake.group.dto.request
 
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
-import jakarta.validation.constraints.Positive
+import jakarta.validation.constraints.PositiveOrZero
 import jakarta.validation.constraints.Size
 
 data class UpdateGroupAppRequest(
     @field:NotNull
-    @field:Positive
+    @field:PositiveOrZero
     val groupAppId: Long,
     @field:NotBlank
     @field:Size(min = 2, max = 10)
