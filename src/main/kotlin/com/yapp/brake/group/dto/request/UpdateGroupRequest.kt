6d198c6a98892fa02f33ac5b294a6a5ea80dd9d0
SeongminKimKR <gvf156@gmail.com>
2025-07-18 00:28:27 +0900

    fix: 중첩 객체 유효성 검증 추가 #50

--- src/main/kotlin/com/yapp/brake/group/dto/request/UpdateGroupRequest.kt
+++ src/main/kotlin/com/yapp/brake/group/dto/request/UpdateGroupRequest.kt
@@ -1,5 +1,6 @@
 package com.yapp.brake.group.dto.request
 
+import jakarta.validation.Valid
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.Size
 
@@ -7,5 +8,6 @@ data class UpdateGroupRequest(
     @field:NotBlank
     @field:Size(min = 2, max = 10)
     val name: String,
+    @field:Valid
     val groupApps: List<UpdateGroupAppRequest> = emptyList(),
 )
