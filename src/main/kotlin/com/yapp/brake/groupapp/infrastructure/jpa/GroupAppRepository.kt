3b1e0890db0b264c6194d27c15bb3f2c6a18dd2e
SeongminKimKR <gvf156@gmail.com>
2025-07-18 22:41:36 +0900

    remove: 네이티브 쿼리 제거 #50

--- src/main/kotlin/com/yapp/brake/groupapp/infrastructure/jpa/GroupAppRepository.kt
+++ src/main/kotlin/com/yapp/brake/groupapp/infrastructure/jpa/GroupAppRepository.kt
@@ -2,13 +2,11 @@ package com.yapp.brake.groupapp.infrastructure.jpa
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Modifying
-import org.springframework.data.jpa.repository.Query
 
 interface GroupAppRepository : JpaRepository<GroupAppEntity, Long> {
     fun findByGroupId(groupId: Long): List<GroupAppEntity>
 
     @Modifying
-    @Query("delete from GroupAppEntity g where g.groupId = :groupId")
     fun deleteByGroupId(groupId: Long): Int
 
     fun deleteByGroupAppIdIn(groupAppIds: List<Long>)
