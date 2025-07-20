d398afe8236ad4199bdf6bc3385358182985ec2e
ay-eonii <naayen@naver.com>
2025-07-20 18:03:14 +0900

    refactor: 변수명 수정

--- src/main/kotlin/com/yapp/brake/session/model/SessionStatistics.kt
+++ src/main/kotlin/com/yapp/brake/session/model/SessionStatistics.kt
@@ -6,14 +6,15 @@ data class SessionStatistics(
     fun update(session: Session): SessionStatistics {
         val updatedMap = statistics.associateBy { it.date }.toMutableMap()
 
-        for (split in session.splitByDate()) {
-            val date = split.start.toLocalDate()
-            val existing =
-                updatedMap[date] ?: DailySessionStatistic(
-                    date = date,
-                    memberId = split.memberId,
-                )
-            updatedMap[date] = existing.add(split)
+        for (dailySession in session.splitByDate()) {
+            val date = dailySession.start.toLocalDate()
+            val dailySessionStatistic =
+                updatedMap[date]
+                    ?: DailySessionStatistic(
+                        date = date,
+                        memberId = dailySession.memberId,
+                    )
+            updatedMap[date] = dailySessionStatistic.add(dailySession)
         }
 
         return SessionStatistics(updatedMap.values.sortedBy { it.date })
