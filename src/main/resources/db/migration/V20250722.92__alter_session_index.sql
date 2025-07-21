18edc4727f0945ecdaf39589772eaf793af8bf8a
ay-eonii <naayen@naver.com>
2025-07-22 02:08:04 +0900

    fix: 외래키 삭제 sql 변경

--- src/main/resources/db/migration/V20250722.92__alter_session_index.sql
+++ src/main/resources/db/migration/V20250722.92__alter_session_index.sql
@@ -1,5 +1,19 @@
-ALTER TABLE session
-DROP FOREIGN KEY session_ibfk_1,
-DROP INDEX session_ibfk_1;
+SELECT constraint_name
+INTO @fk_name
+FROM information_schema.table_constraints
+WHERE table_schema = DATABASE()
+  AND table_name = 'session'
+  AND constraint_type = 'FOREIGN KEY'
+    LIMIT 1;
+
+SET @sql = CONCAT(
+  'ALTER TABLE `session` DROP FOREIGN KEY `',
+  @fk_name,
+  '`;'
+);
+
+PREPARE stmt FROM @sql;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
 
 CREATE INDEX idx_session_member_id ON session (member_id);
