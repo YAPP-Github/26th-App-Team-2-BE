367f9f55d859101d96f06555c3a10edbe7ab9802
SeongminKimKR <gvf156@gmail.com>
2025-08-07 20:03:49 +0900

    refactor: 멀티 모듈 분리 선작업

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,103 +1,120 @@
-import org.springframework.boot.gradle.tasks.bundling.BootJar
-
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
     kotlin("plugin.jpa") version "1.9.25"
     id("org.springframework.boot") version "3.5.0"
     id("io.spring.dependency-management") version "1.1.7"
-    id("org.jlleitschuh.gradle.ktlint").version("12.3.0") // ktlint
-    id("jacoco") // jacoco
-    id("com.epages.restdocs-api-spec") version "0.19.4"
-}
-
-group = "com.brake"
-version = "0.0.1-SNAPSHOT"
-
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
-    }
+    id("org.jlleitschuh.gradle.ktlint").version("12.3.0")
+    id("jacoco")
 }
 
 repositories {
     mavenCentral()
 }
 
+group = "com.brake"
+version = "0.0.1-SNAPSHOT"
+
+subprojects {
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+    apply(plugin = "org.springframework.boot")
+    apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+    apply(plugin = "org.jetbrains.kotlin.plugin.jpa")
+    apply(plugin = "io.spring.dependency-management")
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    apply(plugin = "jacoco")
+
+    repositories {
+        mavenCentral()
+    }
+
+    java {
+        toolchain {
+            languageVersion = JavaLanguageVersion.of(21)
+        }
+    }
+
+    kotlin {
+        compilerOptions {
+            freeCompilerArgs.addAll("-Xjsr305=strict")
+        }
+    }
+
+    ktlint {
+        reporters {
+            reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.JSON)
+        }
+    }
+
+    dependencies {
+        // Kotlin-Logging
+        implementation("io.github.oshai:kotlin-logging-jvm:7.0.3")
+    }
+}
+
 val jwtVersion = "0.12.6"
 extra["springCloudVersion"] = "2025.0.0"
 val flywayVersion = "10.16.0"
 
-dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-
-    // feign
-    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
-
-    // JPA
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-
-    // redis
-    implementation("org.springframework.boot:spring-boot-starter-data-redis")
-
-    // security
-    implementation("org.springframework.boot:spring-boot-starter-security")
-
-    // Mysql
-    runtimeOnly("com.mysql:mysql-connector-j")
-
-    // H2
-    testImplementation("com.h2database:h2")
-
-    // Kotlin-Logging
-    implementation("io.github.oshai:kotlin-logging-jvm:7.0.3")
-
-    // jwt
-    implementation("io.jsonwebtoken:jjwt-api:$jwtVersion")
-    implementation("io.jsonwebtoken:jjwt-impl:$jwtVersion")
-    implementation("io.jsonwebtoken:jjwt-jackson:$jwtVersion")
-
-    // validation
-    implementation("org.springframework.boot:spring-boot-starter-validation")
-
-    // flyway
-    implementation("org.flywaydb:flyway-core:$flywayVersion")
-    implementation("org.flywaydb:flyway-mysql:$flywayVersion")
-
-    // monitoring
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("io.micrometer:micrometer-registry-prometheus")
-
-    // test
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
-
-    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
-    testImplementation("com.epages:restdocs-api-spec-mockmvc:0.19.4")
-}
-
-dependencyManagement {
-    imports {
-        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
-    }
-}
-
-kotlin {
-    compilerOptions {
-        freeCompilerArgs.addAll("-Xjsr305=strict")
-    }
-}
-
-allOpen {
-    annotation("jakarta.persistence.Entity")
-    annotation("jakarta.persistence.MappedSuperclass")
-    annotation("jakarta.persistence.Embeddable")
-}
+// dependencies {
+//    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+//    implementation("org.springframework.boot:spring-boot-starter-web")
+//    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+//    implementation("org.jetbrains.kotlin:kotlin-reflect")
+//
+//    // feign
+//    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+//
+//    // JPA
+//    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+//
+//    // redis
+//    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+//
+//    // security
+//    implementation("org.springframework.boot:spring-boot-starter-security")
+//
+//    // Mysql
+//    runtimeOnly("com.mysql:mysql-connector-j")
+//
+//    // H2
+//    testImplementation("com.h2database:h2")
+//
+//    // Kotlin-Logging
+//    implementation("io.github.oshai:kotlin-logging-jvm:7.0.3")
+//
+//    // jwt
+//    implementation("io.jsonwebtoken:jjwt-api:$jwtVersion")
+//    implementation("io.jsonwebtoken:jjwt-impl:$jwtVersion")
+//    implementation("io.jsonwebtoken:jjwt-jackson:$jwtVersion")
+//
+//    // validation
+//    implementation("org.springframework.boot:spring-boot-starter-validation")
+//
+//    // flyway
+//    implementation("org.flywaydb:flyway-core:$flywayVersion")
+//    implementation("org.flywaydb:flyway-mysql:$flywayVersion")
+//
+//    // monitoring
+//    implementation("org.springframework.boot:spring-boot-starter-actuator")
+//    implementation("io.micrometer:micrometer-registry-prometheus")
+//
+//    // test
+//    testImplementation("org.springframework.boot:spring-boot-starter-test")
+//    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+//    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+//    testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
+//
+//    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+//    testImplementation("com.epages:restdocs-api-spec-mockmvc:0.19.4")
+// }
+//
+// dependencyManagement {
+//    imports {
+//        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+//    }
+// }
 
 jacoco {
     toolVersion = "0.8.13"
@@ -120,71 +137,13 @@ tasks.jacocoTestReport {
     )
 }
 
-openapi3 {
-    setServer("/")
-    title = "Brake API"
-    description = "Brake API description"
-    version = "0.1.0"
-    format = "yaml"
-}
-
-tasks.register("generateSwagger") {
-    dependsOn("openapi3")
-    doFirst {
-        val swaggerUIFile = file("build/api-spec/openapi3.yaml")
-        val securitySchemesContent =
-            """
-              securitySchemes:
-                bearerAuth:
-                  type: http
-                  scheme: bearer
-                  bearerFormat: JWT
-                  name: Authorization
-                  in: header
-                  description: "액세스 토큰을 기입해 주세요"
-            security:
-              - bearerAuth: []
-            """.trimIndent()
-
-        swaggerUIFile.appendText(securitySchemesContent)
-    }
-}
-
-// Swagger 문서 복사
-tasks.register<Copy>("copyToSwagger") {
-    dependsOn("generateSwagger")
-    delete("src/main/resources/static/swagger/openapi3.yaml")
-    copy {
-        from("build/api-spec/openapi3.yaml")
-        into("src/main/resources/static/swagger/")
-    }
-}
-
-tasks.named("bootJar") {
-    dependsOn("copyToSwagger")
-}
-
 tasks.withType<Test> {
     finalizedBy(tasks.jacocoTestReport)
     useJUnitPlatform()
 }
 
-ktlint {
-    reporters {
-        reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.JSON)
-    }
-}
-
 tasks.named<ProcessResources>("processResources") {
     dependsOn("copy main env")
-}
-
-tasks.named<BootJar>("bootJar") {
-    dependsOn("generateSwagger")
-
-    from("build/api-spec/openapi3.yaml") {
-        into("static/swagger")
-    }
 }
 
 tasks.named<ProcessResources>("processTestResources") {
