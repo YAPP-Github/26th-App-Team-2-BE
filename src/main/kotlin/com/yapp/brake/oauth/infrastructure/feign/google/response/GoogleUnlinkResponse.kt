1a5d896884208566d193f55196b2cb4f19e1c202
SeongminKimKR <gvf156@gmail.com>
2025-08-21 22:24:57 +0900

    fix: 불필요 코드 삭제

--- src/main/kotlin/com/yapp/brake/oauth/infrastructure/feign/google/response/GoogleUnlinkResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.yapp.brake.oauth.infrastructure.feign.google.response
-
-import com.fasterxml.jackson.annotation.JsonProperty
-
-data class GoogleUnlinkResponse(
-    @JsonProperty("id")
-    val id: Long,
-)
