ae86c05473d3b1942821dcf5f8ff70045430428c
SeongminKimKR <gvf156@gmail.com>
2025-08-25 23:15:43 +0900

    chore: DataSerializer.kt 필요 의존성 추가

--- brake-common/build.gradle.kts
+++ brake-common/build.gradle.kts
@@ -1,12 +1,11 @@
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
+
     // test
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
 }
 
 tasks {
