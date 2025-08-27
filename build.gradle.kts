6d672d5acd304c52e6270fc6bbe0f21faea37203
SeongminKimKR <gvf156@gmail.com>
2025-08-27 23:02:08 +0900

    fix: remove duplicated plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,6 @@ subprojects {
     apply(plugin = "org.jetbrains.kotlin.jvm")
     apply(plugin = "org.jetbrains.kotlin.plugin.spring")
     apply(plugin = "org.springframework.boot")
-    apply(plugin = "org.jetbrains.kotlin.plugin.spring")
     apply(plugin = "org.jetbrains.kotlin.plugin.jpa")
     apply(plugin = "io.spring.dependency-management")
     apply(plugin = "org.jlleitschuh.gradle.ktlint")
